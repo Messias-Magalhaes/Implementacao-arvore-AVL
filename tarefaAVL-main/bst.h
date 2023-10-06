296395afe6a52bca50e667a5b7d94da1f0d4ad5b
Messias X. Magalhães <112675518+Messias-Magalhaes@users.noreply.github.com>
2023-10-06 13:15:43 -0300

    Add files via upload

---
+++ tarefaAVL-main/bst.h
@@ -0,0 +1,41 @@
+#ifndef BST_H
+#define BST_H
+
+
+typedef struct node * link;
+struct node {
+  link l;
+  link r;
+  int item;
+  int N;      // quantidade de nos na subarvore
+  int id;     // id da nó
+  int height; // altura da subárvore enraizada neste nó
+};
+
+typedef struct tree {
+  link head; // raiz
+  link z;    // nó externo
+} * Tree;
+
+Tree createTree();
+
+
+link rotR (Tree t, link h);
+
+link rotL (Tree t, link h);
+
+link search (Tree h, int query);
+
+link insert (Tree h, int item);
+
+void imprimeEmOrdem (Tree t, link h);
+void imprimePosOrdem (Tree t, link h);
+void imprimePreOrdem (Tree t, link h, int k);
+
+link AVLinsert (Tree t,link h, int item);
+
+void imprime(Tree a, char *s);
+ 
+#endif 
+
+
